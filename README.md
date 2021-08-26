08df511c2fbc7f43c61ec75e5de4725bf888bc66
marcos <marcosperez@Marcos-Perez.local>
2021-08-26 10:18:54 +0200

    Basic models created

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 **LFG API**
+
+ A project to search a party to play games.
+
+It is an API  that uses Django, Django-rest framework, jwt for the authentication and postgres as a database manager.
+
+All the packages needed for the API are in the document "requirements.txt" and can be installed using the command :
+
+`pip -r install requirements.txt`
+
--- api/models.py
+++ api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class Gamer(AbstractUser):
+    username = models.CharField(max_length=40, unique=True, null=True, blank=True)
+    email = models.CharField(unique=True, max_length=100)
+    steam = models.CharField(blank=True, null=True, max_length=100)
+
+    def __str__(self):
+        return f"{self.username}"
+
+
+class Videogame(models.Model):
+    title = models.CharField(max_length=100)
+    genre = models.CharField(max_length=50)
+    description = models.CharField(max_length=400)
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class Party(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
