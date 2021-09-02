1c077bb826149a3ccf6645c6d4e036e569d3d11b
marcos <marcosperez@Marcos-Perez.local>
2021-09-02 11:56:44 +0200

    Models

--- api/models.py
+++ api/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 
 # Create your models here.
 
 class Gamer(AbstractUser):
     username = models.CharField(max_length=40, unique=True, null=True, blank=True)
-    email = models.CharField(unique=True, max_length=100)
     steam = models.CharField(blank=True, null=True, max_length=100)
 
     def __str__(self):
@@ -17,6 +17,7 @@ class Videogame(models.Model):
     title = models.CharField(max_length=100)
     genre = models.CharField(max_length=50)
     description = models.CharField(max_length=400)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.title}"
@@ -24,6 +25,43 @@ class Videogame(models.Model):
 
 class Party(models.Model):
     name = models.CharField(max_length=100)
+    description = models.CharField(max_length=400)
+    videogame = models.ForeignKey(
+        Videogame,
+        on_delete=models.CASCADE,
+        related_name='parties'
+    )
+    creator = models.ForeignKey(Gamer,
+                                on_delete=models.CASCADE,
+                                related_name='parties')
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.name}"
+
+
+class Message(models.Model):
+    id = models.AutoField(primary_key=True)
+    text = models.CharField(max_length=400, null=True)
+    active = models.BooleanField(default=True)
+    writer = models.ForeignKey(Gamer,
+                               on_delete=models.CASCADE,
+                               related_name='messages'
+                               )
+    party = models.ForeignKey(Party,
+                              on_delete=models.CASCADE,
+                              related_name='party_messages'
+                              )
+    created = models.DateTimeField(null=True)
+
+    def save(self, *args, **kwargs):
+        """
+        Used to save timestamps on created
+        """
+        if not self.id:
+            self.created = timezone.now()
+        self.modified = timezone.now()
+        return super(Message, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.text}"
---
+++ api/urls.py
@@ -0,0 +1,5 @@
+from django.urls import include, path, re_path
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
